b63c9f87ddc3131ae7bba0f964a94e316fe76565
José Leal <jose.r.s.leal@gmail.com>
2023-02-22 21:18:46 +0200

    added list elements

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,7 +1,33 @@
 import React from 'react'
 
+const appointmentTimeOfDay = (startsAt) => {
+    const [h, m] = new Date(startsAt)
+        .toTimeString()
+        .split(':');
+    return `${h}:${m}`;
+    // return '${h}:${m}'
+}
+
+
 export const Appointment = ({ customer }) => {
     return (
         <div>{customer.firstName}</div>
     )
 }
+
+export const AppointmentsDayView = ({ appointments }) => {
+    return (
+        <div id="appointmentsDayView">
+            <ol>
+                {appointments.map((a) => (
+
+                    <li key={a.startsAt}>
+                        {appointmentTimeOfDay(a.startsAt)}
+                    </li>
+                ))}
+            </ol>
+        </div >
+    )
+}
+
+
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -3,15 +3,59 @@ import { act } from 'react-dom/test-utils';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { Appointment } from '../src/Appointment';
+import { Appointment, AppointmentsDayView } from '../src/Appointment';
 
+let container;
+beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+});
+
+describe('AppointmentsDayView', () => {
+    const today = new Date();
+    const appointments = [
+        { startsAt: today.setHours(12, 0) },
+        { startsAt: today.setHours(13, 0) }
+    ];
+    const component = (<AppointmentsDayView appointments={appointments} />);
+
+    it('renders a div with right id', () => {
+        const component = (<AppointmentsDayView appointments={[]} />)
+        render(container, component);
+        expect(document.querySelector("div#appointmentsDayView")).not.toBeNull();
+    });
+
+    it('renders an ol element to display appointments', () => {
+        const component = (<AppointmentsDayView appointments={[]} />)
+        render(container, component);
+
+        const listElement = document.querySelector('ol');
+        expect(listElement).not.toBeNull()
+    });
+
+    it('renders an li for each appointment', () => {
+
+        render(container, component);
+
+        const listChildren = document.querySelectorAll('ol > li');
+
+        expect(listChildren).toHaveLength(2);
+
+    });
+
+    it('renders the time of each appointment', () => {
+
+        render(container, component);
+
+        const listChildren = document.querySelectorAll('li');
+
+        expect(listChildren[0].innerHTML).toEqual('12:00');
+        expect(listChildren[1].innerHTML).toEqual('13:00');
+    });
+});
 
 describe('Appointment', () => {
-    let container;
-    beforeEach(() => {
-        container = document.createElement("div");
-        document.body.replaceChildren(container);
-    });
+
     it.skip('renders the customer 1st name', async () => {
         global.IS_REACT_ACT_ENVIRONMENT = false;
         const customer = { firstName: "Ashley" };
